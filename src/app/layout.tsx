fb5441e28c11ca4dbf7f3d1400a19729e3dd9465
ariesjace <ariesjaceu@gmail.com>
2025-09-26 14:14:23 +0800

    change font to IBM plex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,36 +1,34 @@
+// src/app/layout.tsx
 import type { Metadata } from "next";
-import { Trispace } from "next/font/google";
+import { Noto_Serif } from "next/font/google"; // Import Noto Serif
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-const trispace = Trispace({
+const notoSerif = Noto_Serif({
+  weight: ["400"], // Regular
   subsets: ["latin"],
+  display: "swap",
 });
 
 export const metadata: Metadata = {
   title: "floodwatch.ph",
   description: "Real-Time Flood News for Every Filipino.",
-  icons:{
-    icon:[{url:'/img/floodwatch.ph.png'}]
-  },
-  openGraph:{
-    title: 'floodwatch.pg',
+  icons: { icon: [{ url: "/img/floodwatch.ph.png" }] },
+  openGraph: {
+    title: "floodwatch.ph",
     description: "Real-Time Flood News for Every Filipino.",
-    type: 'article',
-    authors: 'ariesjace',
+    type: "website",
+    authors: "ariesjace",
   },
-  keywords: ['floodwatch.ph', 'news', 'news about floods']
+  keywords: ["floodwatch.ph", "news", "news about floods"],
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={trispace.className}>
+      {/* Apply Noto Serif globally */}
+      <body className={`${notoSerif.className} antialiased`}>
         <main className="px-2 md:px-20">
           <Header />
           <section className="flex justify-between">
